a05c5a4a4166cbaf61858ad897d4c026b4fe41bf
VirtualZero <birtchum@virtualzero.net>
2019-03-10 15:49:21 -0400

    Update debian-triple-s.sh

--- debian-triple-s.sh
+++ debian-triple-s.sh
@@ -2,7 +2,7 @@
 
 create_cert() {
     openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt;
-    openssl dhparam -out /etc/nginx/dhparam.pem 4096;
+    openssl dhparam -dsaparam -out /etc/nginx/dhparam.pem 4096;
 }
 
 create_config_files() {
